2ee4c6b3c81e8b16f4f6383d6abe3b2073395236
ItzBleaZer <bleazeroff6@gmail.com>
2025-11-14 16:03:28 +0400

    feat: features/brand/start

---
+++ src/components/Brands/Brands.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import brand from "../../asets/img/brand.png"o
+
+const Brands = () => {
+  return (
+    <img src="../brand_1.png" alt="" />
+  )
+}
+
+export default Brands
