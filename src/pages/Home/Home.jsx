e7d26ef5654ad55837c778229531d02e53060cff
ArmMelqonyan <armanmelqonyan257@gmail.com>
2025-11-24 15:12:26 +0400

    feat: MainForm End

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import s from "./Home.module.sass"
+import MainForm from '../../components/MainForm/MainForm'
 
 const Home = () => {
   return (
     <div>
-
+      <MainForm/>
     </div>
   )
 }
