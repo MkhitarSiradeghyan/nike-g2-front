489dea18f50e67c015436816193db5114db0857f
gharibyan033 <gharib7an@gmail.com>
2025-11-24 15:41:41 +0400

    feat: Banner done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
+import Banner from '../../components/Banner/Banner'
 
 const Home = () => {
   return (
-    <Hero/>
+    <Banner/>
   )
 }
 
