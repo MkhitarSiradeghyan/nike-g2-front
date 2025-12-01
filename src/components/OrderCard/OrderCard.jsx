a1c6e884d57d9379ffd12d67443923a19bde3029
ArmMelqonyan <armanmelqonyan257@gmail.com>
2025-12-01 10:48:56 +0400

    feature/Order-card

---
+++ src/components/OrderCard/OrderCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import s from './OrderCard.module.sass'
+import { FaArrowRightLong } from "react-icons/fa6"
+
+
+const OrderCard = () => {
+    return (
+        <div className={s.ordercard}>
+            <h3 className={s.ordertext}>order summary</h3>
+            <div className={s.subtutal}>
+                <div className={s.text}>subtutal</div>
+                <div className={s.monay}>$565</div>
+                </div>
+            <div className={s.discount}>
+                <div className={s.text}>discount (-20%)</div>
+                <div className={s.monay1}>-$113</div>
+                </div>
+            <div className={s.delivery}>
+                <div className={s.text}>delivery fee</div>
+                <div className={s.monay}>$15</div>
+                </div>
+                <hr />
+                <div className={s.total}>
+                    <h4 className={s.text1}>total</h4>
+                    <div className={s.monay2}>$467</div>
+                </div>
+                <div className={s.inp}>
+                    <input className={s.input} type="text"  placeholder='Add promo code'/>
+                    <button className={s.btn}>Apply</button>
+                </div>
+               <button className={s.btn1}>Go to Checkout <FaArrowRightLong /></button>
+              
+        </div>
+    )
+}
+
+
+export default OrderCard
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Hero from '../../components/Hero/Hero'
-import Banner from '../../components/Banner/Banner'
+import OrderCard from '../../components/OrderCard/OrderCard'
+
+
 
 const Home = () => {
   return (
-    <Banner/>
+    <OrderCard/>
   )
 }
 
