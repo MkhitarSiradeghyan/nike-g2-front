2d4da5aaa505f216bafe6612a75860d198b8eba8
ItzBleaZer <bleazeroff6@gmail.com>
2025-12-08 15:54:48 +0400

    feat: bre

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import OrderCard from '../../components/OrderCard/OrderCard'
+import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs'
 
 
 
 const Home = () => {
   return (
-    <OrderCard/>
+    <Breadcrumbs/>
   )
 }
 
